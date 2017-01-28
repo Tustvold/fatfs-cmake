29ddac9a29e04de265707513945e5ad14c78e4b0
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2017-01-28 20:31:05 +0000

    Added CMake Support

--- fatfs.mk
+++
@@ -1,7 +0,0 @@
-# FATFS files.
-FATFSSRC = ${CHIBIOS}/os/various/fatfs_bindings/fatfs_diskio.c \
-           ${CHIBIOS}/os/various/fatfs_bindings/fatfs_syscall.c \
-           ../../shared/fatfs/src/ff.c \
-           ../../shared/fatfs/src/option/unicode.c
-
-FATFSINC = ../../shared/fatfs/src
